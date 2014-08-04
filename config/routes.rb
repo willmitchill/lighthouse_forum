4a54c8f897058717dea51c82f5e74aa57a6b97a6
Will <wmitchel@hotmail.com>
2014-08-04 14:42:23 -0700

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 LighthouseForum::Application.routes.draw do
-  get 'posts',          to: 'posts#index'
-  get 'posts/new',      to: 'posts#new'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id',      to: 'posts#show', as: 'post' # necessary for the update action!
-  patch 'posts/:id',    to: 'posts#update'
-  post 'posts',         to: 'posts#create'
+  resources :posts
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
